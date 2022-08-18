620b5253478e6d74b150c33ea8b532d3f163630e
Nosi <nosichefane@gmail.com>
2022-08-18 07:07:34 +0200

    loan prediction form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Loan Prediction Form</h1>
+      <form action="">
+        <label htmlFor="ApplicantIncome">Applicant Income
+        <input type="number"  className="form-input"/>  
+        </label>
+
+        <label htmlFor="CoapplicantIncome">Co-Applicant Income
+        <input type="number"  className="form-input"/>  
+        </label>
+
+        <label htmlFor="LoanAmount">Loan Amount
+        <input type="number"  className="form-input"/>  
+        </label>
+        
+        <label htmlFor="LoanAmountTerm">Loan Amount Term
+        <input type="number"  className="form-input"/>  
+        </label>
+
+        <label>Credit History
+        <select  className="form-input">
+          <option value="Yes">Yes</option>
+          <option value="No">No</option>  
+        </select> 
+        </label>
+
+        <label>Self-employed
+        <select  className="form-input">
+          <option value="Yes">Yes</option>
+          <option value="No">No</option>  
+        </select> 
+        </label>
+
+        <label>Property Area
+        <select  className="form-input">
+          <option value="urban">Urban</option>
+          <option value="rural">Rural</option>  
+          <option value="semi-urban">Semi-Urban</option>
+        </select> 
+        </label>
+
+        <label>Married
+        <select  className="form-input">
+          <option value="Married">Yes</option>
+          <option value="Not Married">No</option>  
+        </select> 
+        </label>
+
+        <label>Education
+        <select  className="form-input">
+          <option value="graduate">Graduate</option>
+          <option value="Not graduate">Not Graduate</option>  
+        </select> 
+        </label>
+
+        <label>Gender
+        <select className="form-input">
+          <option value="male">Male</option>
+          <option value="female">Female</option>  
+        </select> 
+        </label>
+
+        <button type="submit">Predict</button>
+      </form>
+        
     </div>
   );
 }
